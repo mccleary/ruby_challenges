f261239ec5b9a1026413326669da583e784dba2f
Trista McCleary <trista_mccleary@yahoo.com>
2018-04-18 01:13:41 -0400

    initial commit

---
+++ weather.rb
@@ -0,0 +1,18 @@
+weather = "sunny"
+
+case weather
+when "cloudy"
+	puts "It's cloudy. Be sure to take a sweater"
+when "foggy"
+	puts "Drive safe, it's foggy out there!"
+when "rainy"
+	puts "It's raining. Don't forget your umbrella!"
+when "snowing"
+  puts "It's snowing! Don't forget your boots!"  
+when "sunny"
+	puts "It's very sunny today. Don't forget your sunglasses."
+when "cold"
+	puts "It's cold. Don't forget your coat!"
+else
+  puts "Hmm, Best to be prepared for anything "
+end
